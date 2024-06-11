a64a5da6d527343ab96a06e8ce2cdf8d6a3c2d57
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-06-11 14:11:26 +0200

    Exercise 3 done

--- ex3.js
+++ ex3.js
@@ -8,12 +8,12 @@ app.use(express.json());
 let products = {
   description: "Productos",
   items: [
-    { id: 1, nombre: "Taza de Harry Potter", precio: 300 },
-    { id: 2, nombre: "FIFA 22 PS5", precio: 1000 },
-    { id: 3, nombre: "Figura Goku Super Saiyan", precio: 100 },
-    { id: 4, nombre: "Zelda Breath of the Wild", precio: 200 },
-    { id: 5, nombre: "Skin Valorant", precio: 120 },
-    { id: 6, nombre: "Taza de Star Wars", precio: 220 },
+    { id: 1, name: "Taza de Harry Potter", price: 300 },
+    { id: 2, name: "FIFA 22 PS5", price: 1000 },
+    { id: 3, name: "Figura Goku Super Saiyan", price: 100 },
+    { id: 4, name: "Zelda Breath of the Wild", price: 200 },
+    { id: 5, name: "Skin Valorant", price: 120 },
+    { id: 6, name: "Taza de Star Wars", price: 220 },
   ],
 };
 
@@ -23,11 +23,11 @@ app.get("/products", (req, res) => {
 });
 
 app.post("/addProduct", (req, res) => {
-  const { nombre, precio } = req.body;
+  const { name, price } = req.body;
   products.items.push({
     id: products.items.at(-1).id + 1,
-    nombre,
-    precio: +precio,
+    name,
+    price: +price,
   });
 
   res.status(201).send(products);
@@ -35,15 +35,15 @@ app.post("/addProduct", (req, res) => {
 
 app.put("/updateProduct/id/:id", (req, res) => {
   const id = req.params.id;
-  const { nombre, precio } = req.body;
+  const { name, price } = req.body;
   let status = 400;
 
-  if (!nombre || !precio) return res.status(400).send();
+  if (!name || !price) return res.status(400).send();
 
   products.items.forEach((item) => {
     if (item.id === +id) {
-      item.nombre = nombre ? nombre : item.nombre;
-      item.precio = precio ? precio : item.precio;
+      item.name = name ? name : item.name;
+      item.price = price ? price : item.price;
       status = 200;
     }
   });
@@ -61,6 +61,38 @@ app.delete("/deleteProduct/id/:id", (req, res) => {
   status === 200 ? res.status(200).send(products) : res.status(400).send();
 });
 
+// GETTERS
+app.get("/getProductsByPrice/price/:price", (req, res) => {
+  const price = +req.params.price;
+  let result = products.items.filter((item) => item.price === price);
+  res.status(result.length > 0 ? 200 : 400).send(result);
+});
+
+app.get("/getProductsByPrice", (req, res) => {
+  const minPrice = +req.query.minPrice;
+  const maxPrice = +req.query.maxPrice;
+  let result = products.items.filter(
+    (item) => item.price >= minPrice && item.price <= maxPrice
+  );
+
+  res.status(result.length > 0 ? 200 : 400).send(result);
+});
+
+app.get("/getProduct/id/:id", (req, res) => {
+  const id = +req.params.id;
+  const result = products.items.find((item) => item.id === id);
+
+  res.status(result ? 200 : 400).send(result);
+});
+
+app.get("/getProduct/name/:name", (req, res) => {
+  const name = req.params.name;
+  const result = products.items.find((item) => item.name === name);
+
+  res.status(result ? 200 : 400).send(result);
+});
+
+// PORT
 app.listen(PORT, () => {
   console.log(`Server listening to port ${PORT}`);
 });
